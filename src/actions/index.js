bb0b9457e206c579d16bf64708904a3b23317fbe
BrenoTeixeira <brenotx@gmail.com>
2017-05-16 09:05:28 -0300

    Final commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,8 @@ const API_KEY = '?key=AIzaSyDI5VosGhWFzCci1FNGiv5FlK3vlfzEqrI';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -20,6 +22,24 @@ export function createPost(props) {
 
     return {
         type: CREATE_POST,
+        payload: request
+    };
+}
+
+export function fetchPost(id) {
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+    return {
+        type: FETCH_POST,
+        payload: request
+    };
+}
+
+export function deletePost(id) {
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+    return {
+        type: DELETE_POST,
         payload: request
     };
 }
